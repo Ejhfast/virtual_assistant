0e03ba055260134e2c8a900025fd89bdd8099b69
Sharon Zhou <sharonzhou@DN0a22a232.SUNet>
2016-07-21 15:18:42 -0700

    added texting sarah at a certain frequency and scheduling an arbitrary event to safeway in calendar

--- sample-modules.py
+++ sample-modules.py
@@ -2,10 +2,12 @@ from core import Assistant
 import sys
 import random
 import math
+import time
+import datetime
 
 assistant = Assistant()
 
-# turn on Flux when I turn off Word
+# python sample-modules.py 'turn on Flux when I turn off Word'
 @assistant.register("open {app}")
 def open(app):
 	print("Opened", app)
@@ -26,6 +28,7 @@ def listen(app):
 
 @assistant.register("turn {state1} {app1} when I turn {state2} {app2}")
 def toggleWithChangedState(state1, app1, state2, app2):
+	print(state1, app1, state2, app2)
 	while state1 not in ["on","off"]:
 		state1 = input("Would you like to turn " + app1 + " on or off? ")
 	param = "listen to " + app2
@@ -43,7 +46,7 @@ def toggleWithChangedState(state1, app1, state2, app2):
 		assistant.execute(param)
 	return
 
-# wake me up in 5 min
+# python sample-modules.py 'wake me up in 5 min'
 @assistant.register("set countdown for {time}")
 def setTimer(time):
 	print("Setting countdown timer (in ms) for", time)
@@ -71,20 +74,78 @@ def alarm(n, unit):
 	assistant.execute("trigger alarm")
 	return
 
-# tell Sarah I love you everyday at 5pm in different ways
-@assistant.register("tell {person} {message} {frequency} at {time} in different ways")
-def repeatedText(person, message, frequency, time):
+# python sample-modules.py 'text Sarah "I love you" everyday at 5pm in 6 different ways'
+@assistant.register("what is phone number of {person}?")
+def getContactNumber(person):
+	print("Looking up phone number of " + person)
+	number = '555-555-5555'
+	return number
+
+@assistant.register("another way to say {string}")
+def synonyms(string):
+	print("Getting synonyms of " + string)
+	synonyms = ["I love you", "love you", "I love you!", "I love you <3", "I <3 you", "ilu"]
+	return synonyms
+
+@assistant.register("time object of {string}")
+def convertTime(string):
+	print("Detecting time format of " + string)
+	print("Converting into time object from string")
+	return string
+
+@assistant.register("send text {message} to {person}")
+def sendSMS(message, person):
+	number = getContactNumber(person)
+	return(print("Sending text with message " + message + " to " + person + " at " + number))
+
+@assistant.register("do {action} at this {time} {frequency}")
+def scheduleCronJob(action, time, frequency):
+	print(" is scheduled for " + time + " at frequency of " + frequency)
+
+@assistant.register("text {person} {message} {frequency} at {time} in {n} different ways")
+def repeatedText(person, message, frequency, time, n):
+	texts = synonyms(message)
+	times = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	for i in range(len(texts)):
+		k = i % len(times)
+		t = time + " " + times[k]
+		j = i % len(texts)
+		text = texts[j]
+		scheduleCronJob(sendSMS(text, person), t, "every " + str(len(texts)) + " days")
 
 
-# when should I go to Safeway? 
-#ask: where are you now? determine distance + calendar sync to find when available
-@assistant.register("when should I go to {location}")
+# when should I go to Safeway for 20 min? 
+# assumes using current location for origin... hmmm not the event before
+def scheduleEvent(name, time, duration):
+	print("Scheduling " + name + " at " + time + " for " + str(duration) + " min")
 
+def findFreeTimeSlots(duration):
+	print("Finding free time slots available for " + str(duration) + " min")
+	slots = ["Tomorrow at 12pm", "Friday at 4pm", "Saturday at 9am"]
+	return slots
 
-# Notify people I’m meeting with later if I’m going to be late
-#detect invites to events in calendar, detect where I am now and distance from the event location;
-# if past time that I can get there on time, text/email the invitees
-@assistant.register("tell people I'm meeting with later if I'm going to be late")
+def scheduleFreeTime(name, duration):
+	freeTimes = findFreeTimeSlots(duration)
+	scheduleEvent(name, freeTimes[0], duration)
+	print("Scheduled event " + name + " in 1st free time slot at " + freeTimes[0] + " for " + str(duration) + " min")
+
+def getCurrentLocation():
+	print("Finding current location")
+	coordinates = "home"
+	return coordinates
+
+def distance(origin, destination):
+	print("Finding distance between " + origin + " and " + destination)
+	distance = 5
+	return distance
+
+@assistant.register("when should I go to {location} for {duration} min?")
+def scheduleErrand(location, duration):
+	origin = getCurrentLocation()
+	travelDistance = distance(origin, location)
+	speed = 10
+	travelTime = 2 * travelDistance * speed
+	scheduleFreeTime("go to " + location, travelTime + duration)
 
 
 print(assistant.execute(sys.argv[1]))
