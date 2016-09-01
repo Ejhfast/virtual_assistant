486003b1315fc93b33ce19c7e92215097123dc43
Sharon Zhou <sharonzhou@DN0a22a775.SUNet>
2016-08-31 17:47:14 -0700

    sending all vars to js

--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,53 @@
-$(document).ready(function(){
-	print('hello')
-	$('input[name="arg"]').blur(function() {
-		if(!this.value) {
-			$(this).addClass('empty_warning');
+function validateInput() {
+	$('input[name="arg"]').each(function() {
+		if (!$(this).val()) {
+			$(this).addClass('empty_warning');		
+		} else {
+			$(this).removeClass('empty_warning');
 		}
-	});
+	})
+}
+
+function removeHighlight() {
+	$('.command').each(function() {
+		$(this).removeClass('highlight');
+		$('#no_command').removeClass('highlight');
+	})	
+}
+
+function chooseCommand(cmd) {
+	removeHighlight();
+	cmd.addClass('highlight');
+
+	// get args
+}
+
+function findCommand() {
+	removeHighlight();
+	$('#no_command').addClass('highlight');
+	
+	// add / append input box inside div
+
+}
+
+function test_func(data) {
+    console.log(data);
+}
+
+$(document).ready(function() {
+	display_data = $('#data').data();
+	console.log(display_data);
+
+	$('input[name="args_submit"]').click(function() {
+		validateInput();
+	})
+
+	$('.command').click(function() {
+		chooseCommand($(this));
+	})
+
+	$('#no_command').click(function() {
+		findCommand();
+	})
+
 })
--- server.py
+++ server.py
@@ -15,20 +15,26 @@ async def user_input(request):
 	# register commands
 	assistant = demo_commands.commands(Assistant())
 	
+	display_data = {'user_input': user_input}
+
 	# if direct match, execute the query
 	is_match, vals = assistant.execute(user_input)
 	if is_match:
-		return {'user_input': user_input, 'is_match': is_match, 'vals': vals}
+		display_data.update({'is_match': is_match, 'vals': vals})
+		return {'display_data': display_data}
 	
 	is_confident, cmds = assistant.predict(user_input)
+	display_data.update({'is_confident': is_confident, 'cmds': cmds})
+
 	if is_confident:
 		cmd = cmds[0]
 
 		# extract required parameters to ask user to specify
 		args = assistant.mappings[cmd]['args']
 
-		return {'user_input': user_input, 'is_confident': is_confident, 'cmds': cmds, 'cmd': cmd, 'args': args}
-	return {'user_input': user_input, 'is_confident': is_confident, 'cmds': cmds}
+		display_data.update({'cmd': cmd, 'args': args})
+		return {'display_data': display_data}
+	return {'display_data': display_data}
 
 async def event_trigger(request):
 	data = await request.json()
@@ -36,6 +42,9 @@ async def event_trigger(request):
 	return web.Response()
 
 app = web.Application()
+app.router.add_static('/js', 'js/')
+app.router.add_static('/css', 'css/')
+app.router.add_static('/fonts', 'fonts/')
 app.router.add_route('GET', '/', handle)
 app.router.add_route('POST', '/user_input', user_input)
 app.router.add_route('POST', '/event_trigger', event_trigger)
